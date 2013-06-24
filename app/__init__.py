a2ccbed6bf863ecb7520ad88c2d54a09d8004b69
foulas <rohithkishan.krs@gmail.com>
2013-06-24 11:33:49 +0530

    login complete

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,18 @@
 from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
+import os
+from flask.ext.login import LoginManager
+# from flask.ext.openid import OpenID
+from config import basedir
+
+
 app = Flask(__name__)
-from app import views
+app.config.from_object('config') #config file used.
+db = SQLAlchemy(app) #database object
+
+lm = LoginManager()
+lm.init_app(app)
+lm.login_view = 'login'
+# oid = OpenID(app, os.path.join(basedir, 'tmp'))
+
+from app import views, models
--- app/views.py
+++ app/views.py
@@ -1,10 +1,19 @@
-from flask import render_template
-from app import app
+from flask import render_template, flash, redirect, session, url_for, request, g
+from flask.ext.login import login_user, logout_user, current_user, login_required
+from app import app, db, lm
+from forms import LoginForm
+from models import User, ROLE_USER, ROLE_ADMIN
+
+@app.before_request
+def before_request():
+    g.user = current_user
+
 
 @app.route('/')
 @app.route('/index')
+@login_required
 def index():
-     user = { 'nickname': 'Miguel' } # fake user
+     user = g.user
      posts = [ # fake array of posts
          { 
            'author': { 'nickname': 'John' }, 
@@ -19,3 +28,34 @@ def index():
          title = 'Home', user = user,
          posts = posts)
    
+@app.route('/login', methods = ['GET', 'POST'])
+def login():
+    if g.user is not None and g.user.is_authenticated():
+        return redirect(url_for('index'))
+    form = LoginForm()
+    if form.validate_on_submit():  #goes to /index if form input is validated.
+            session['remember_me'] = form.remember_me.data
+            # return oid.try_login(form.openid.data, ask_for = ['nickname', 'email'])
+            uName = form.username.data
+            user = User.query.filter_by(nickname = uName).first()
+            if user is None:
+                flash('Invalid login. Please try again')
+                return redirect(url_for('login'))
+            remember_me = False
+            if 'remember_me' in session:
+                remember_me = session['remember_me']
+                session.pop('remember_me', None)
+            login_user(user,remember = remember_me)
+            return redirect(request.args.get('next') or url_for('index'))
+    return render_template('login.html', 
+        title = 'Sign In',
+        form = form)
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+@lm.user_loader  #function to load user from the database.
+def load_user(id):
+    return User.query.get(int(id))
